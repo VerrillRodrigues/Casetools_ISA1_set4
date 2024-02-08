165e109564bf6989fac9b4ac79820f4106a54eec
VerrillRodrigues <rbca22238.rcca@gmail.com>
2024-02-08 10:21:38 +0530

    commit first

---
+++ Casetools_ISA1_set4/src/javaproject/Circle.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+public class Circle {
+	private double radius;
+	public Circle(double radius) {
+		this.radius=radius;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius=radius;
+	}
+}
